febf0f26d9446606bbcb9c01e55af9bb80c292e1
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-01-29 09:17:59 -0300

    Corrido: lista de REs processando dados duplicados

--- Main.py
+++ Main.py
@@ -15,14 +15,14 @@ def main():
     if not all([conferencia, contas, dependentes, eSocial,workForce, checkList]):
         print('Algumas planilhas não foram encontradas. Verifique os nomes dos arquivos.')
         return
-
-    for re in checkList['B'][2:]:
-        resConferidos = []
-        nomesConferidos = []
-        if re.value is not None:
-            for celula in conferencia['A'][1:]:
-                if celula.value == re.value:
-                    if re.value not in resConferidos:
+    
+    resConferidos = [] # Armazena o RE para evitar duplicidade
+    for re in checkList['B'][2:]: # Para linha na Coluna B a partir da terceira linha:
+        nomesConferidos = [] # Armazena os nomes dos dependentes do RE que está sendo conferido para evitar duplicidade
+        if re.value is not None: # Caso o valor do RE não seja vazio
+            for celula in conferencia['A'][1:]: # Verifica se o valor do RE a ser conferido é igual ao RE dentro da tabela
+                if celula.value == re.value: # Caso seja igual
+                    if re.value not in resConferidos: # E não esteja duplicado
                         resConferidos.append(re.value)
                         array = SheetsController.get_data(celula.row, conferencia, contas, dependentes, eSocial, workForce, re.value, nomesConferidos)
                         SheetsController.fill_sheet(checkList, re.row, array)
