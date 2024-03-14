6fcede29cd9d8444ce42883786f9328063c9f058
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-03-14 13:32:15 -0300

    Código pronto para release

--- src/controllers/docusign.py
+++ src/controllers/docusign.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import ElementClickInterceptedException
 import time
 import os
+import sys
+sys.path.append(os.getcwd())
 
 class DocusignController:
     def __init__(self, email, senha):
